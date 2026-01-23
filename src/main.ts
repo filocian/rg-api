5bdd4a232bb00fd6ed1fcec22cc07ed5edba3600
messagenes <165073282+messagenes@users.noreply.github.com>
2026-01-23 12:45:09 +0100

    Fix typescript types

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
-import { Hono } from 'npm:hono';
+import { Hono, type Context } from 'npm:hono';
 
 const app = new Hono();
 
-app.get('/', (c) => {
+app.get('/', (c: Context) => {
     return c.text('Hello from rg-api (Hono on Deno!)');
 });
 
 // KV Example (Local Simulation)
-app.get('/kv-demo', async (c) => {
-    const kv = await Deno.openKv(Deno.env.get("DENO_KV_PATH"));
+app.get('/kv-demo', async (c: Context) => {
+    const kvPath = Deno.env.get("DENO_KV_PATH");
+    const kv = await Deno.openKv(kvPath);
     await kv.set(["demo"], "it works");
     const res = await kv.get(["demo"]);
     return c.json({ result: res.value });
